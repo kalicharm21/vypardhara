bb50351573877575071fb2690c5b95c6f61c9087
Ishaan Mittal <ishaanmittal999@gmail.com>
2026-05-07 21:11:43 +0530

    Fixed import path

--- src/lib/types.ts
+++
@@ -1,7 +0,0 @@
-export interface SignalBreakdown {
-  name: number | boolean | string;
-  address: number | boolean | string;
-  pan: number | boolean | string;
-  gstin: number | boolean | string;
-  intra_dept: number | boolean | string;
-}
