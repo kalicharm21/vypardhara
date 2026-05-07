95239e8a74ef02d928ab4eba5d981ea01e8522f9
Ishaan Mittal <ishaanmittal999@gmail.com>
2026-05-07 21:08:41 +0530

    Changed type file

---
+++ src/lib/types.ts
@@ -0,0 +1,7 @@
+export interface SignalBreakdown {
+  name: number | boolean | string;
+  address: number | boolean | string;
+  pan: number | boolean | string;
+  gstin: number | boolean | string;
+  intra_dept: number | boolean | string;
+}
